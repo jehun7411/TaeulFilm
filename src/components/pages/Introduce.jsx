a4af810b32a73f808e341c5951040a44dcf610d7
geon <ehdrjs4046@gmail.com>
2021-10-08 19:37:08 +0900

    [ Feat : new ] deving routing page

---
+++ src/components/pages/Introduce.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Introduce() {
+  return <div>회사 소개</div>;
+}
+
+export default Introduce;
---
+++ src/components/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Introduce } from "./Introduce";
+export { default as Main } from "./Main";
+export { default as Product } from "./Product";
+export { default as Faq } from "./Faq";
+export { default as Business } from "./Business";
