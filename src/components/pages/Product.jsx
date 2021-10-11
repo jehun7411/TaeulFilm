2ab771e121df5849b1e938991ceeeb9879aa70e6
ohjehun <jehun7411@gmail.com>
2021-10-12 01:26:34 +0900

    pituer

--- src/components/pages/Product.jsx
+++ src/components/pages/Product.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import introuduce_image from "../../asset/images/introuduce_image.png";
+import Inner from "../atoms/Inner";
+const ProductLogo = styled.h1`
+margin-top: 50px;
+width: 100%;
+height: 50px;
+line-height: 50px;
+text-align: center;
+font-size: 1.875rem;
+
+
+& a {
+    font-family: "Amita", cursive;
+    color: #212529;
+    text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000,
+      1px 1px 0 #000;
+  }
+`;
+  
+const IntroduceImage = styled.section`
+width: 100%;
+min-height: 308px;
+position: relative;
+background: url(${introuduce_image}) no-repeat;
+`;
 
 function Product() {
-  return <div>제품 소개</div>;
+  return <div>
+    <Inner>
+    <ProductLogo>
+      
+    </ProductLogo>
+    </Inner>
+  </div>;
 }
 
 export default Product;
