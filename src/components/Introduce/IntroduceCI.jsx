77aff0f1b0ff7392ef044aed9dc51db23fca98c5
geon <ehdrjs4046@gmail.com>
2021-10-15 16:59:39 +0900

    [ Refactor : revive ]   Change Conditional operator
    Revive based on review : Change Conditional operator

--- src/components/Introduce/IntroduceCI.jsx
+++ src/components/Introduce/IntroduceCI.jsx
@@ -28,17 +28,19 @@ const CIIconImage = styled.div`
   width: 80px;
   height: 80px;
   ${({ number }) => {
-    return number === 1
-      ? css`
-          background: url(${service}) center center/contain no-repeat;
-        `
-      : number === 2
-      ? css`
-          background: url(${devlop}) center center/contain no-repeat;
-        `
-      : css`
-          background: url(${cooperation}) center center/contain no-repeat;
-        `;
+    if (number === 1) {
+      return css`
+        background: url(${service}) center center/contain no-repeat;
+      `;
+    } else if (number === 2) {
+      return css`
+        background: url(${devlop}) center center/contain no-repeat;
+      `;
+    } else if (number === 3) {
+      return css`
+        background: url(${cooperation}) center center/contain no-repeat;
+      `;
+    }
   }}
 `;
 const CITextWrap = styled.div`
@@ -63,51 +65,53 @@ function IntroduceCI() {
   return (
     <CISection>
       <Title>경영 이념</Title>
-      <Icon>
-        <CIIconWrap>
-          <CIIconImage number={1} />
-          <CITextWrap>
-            <CIText1>
-              지속적인 서비스 개선
-              <br />
-            </CIText1>
-            <CIText2>
-              주기적인 팀회의를 통해 보다
-              <br />질 높은 서비스 제공
-            </CIText2>
-          </CITextWrap>
-        </CIIconWrap>
-      </Icon>
-      <Icon2>
-        <CIIconWrap>
-          <CIIconImage number={2} />
-          <CITextWrap>
-            <CIText1>
-              개개인의 성장
-              <br />
-            </CIText1>
-            <CIText2>
-              자격증 및 온라인 강의 수강
-              <br />
-              비용 및 시간 적극 지원
-            </CIText2>
-          </CITextWrap>
-        </CIIconWrap>
-        <CIIconWrap>
-          <CIIconImage number={3} />
-          <CITextWrap>
-            <CIText1>
-              개개인의 성장
-              <br />
-            </CIText1>
-            <CIText2>
-              주마다 갖는 회의와 자유로운 분위기
-              <br />
-              에서의 아이디어 창출 및 팀워크 상승
-            </CIText2>
-          </CITextWrap>
-        </CIIconWrap>
-      </Icon2>
+      <div>
+        <Icon>
+          <CIIconWrap>
+            <CIIconImage number={1} />
+            <CITextWrap>
+              <CIText1>
+                지속적인 서비스 개선
+                <br />
+              </CIText1>
+              <CIText2>
+                주기적인 팀회의를 통해 보다
+                <br />질 높은 서비스 제공
+              </CIText2>
+            </CITextWrap>
+          </CIIconWrap>
+        </Icon>
+        <Icon2>
+          <CIIconWrap>
+            <CIIconImage number={2} />
+            <CITextWrap>
+              <CIText1>
+                개개인의 성장
+                <br />
+              </CIText1>
+              <CIText2>
+                자격증 및 온라인 강의 수강
+                <br />
+                비용 및 시간 적극 지원
+              </CIText2>
+            </CITextWrap>
+          </CIIconWrap>
+          <CIIconWrap>
+            <CIIconImage number={3} />
+            <CITextWrap>
+              <CIText1>
+                개개인의 성장
+                <br />
+              </CIText1>
+              <CIText2>
+                주마다 갖는 회의와 자유로운 분위기
+                <br />
+                에서의 아이디어 창출 및 팀워크 상승
+              </CIText2>
+            </CITextWrap>
+          </CIIconWrap>
+        </Icon2>
+      </div>
     </CISection>
   );
 }
