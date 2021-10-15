cbbe88c88c6076a18e57b4e6a9a4aa0477159164
KyeongBin <aa0715@naver.com>
2021-10-15 14:41:07 +0900

    [Feat : revive] : Revive based on review : Change naming

--- src/components/Introduce/IntroduceCI.jsx
+++ src/components/Introduce/IntroduceCI.jsx
@@ -5,28 +5,29 @@ import service from "../../asset/images/service.png";
 import devlop from "../../asset/images/devlop.png";
 import cooperation from "../../asset/images/cooperation.png";
 
-const CISection = styled.div`
+const CiSection = styled.div`
   padding-top: 50px;
 `;
-const Icon = styled.div`
+const IconWrap = styled.div`
   display: flex;
   justify-content: center;
 `;
-const Icon2 = styled.div`
+const IconWrap2 = styled.div`
   display: flex;
   justify-content: center;
   gap: 20%;
 `;
-const CIIconWrap = styled.div`
+const CiIconWrap = styled.div`
   display: flex;
   justify-content: center;
   margin-top: 50px;
   width: 300px;
   height: 80px;
 `;
-const CIIconImage = styled.div`
+const CiIconImage = styled.div`
   width: 80px;
   height: 80px;
+
   ${({ number }) => {
     return number === 1
       ? css`
@@ -41,18 +42,18 @@ const CIIconImage = styled.div`
         `;
   }}
 `;
-const CITextWrap = styled.div`
+const CiTextWrap = styled.div`
   padding-top: 10px;
   width: 220px;
   height: 80px;
 `;
-const CIText1 = styled.p`
+const CiText1 = styled.p`
   font-weight: 500;
   font-size: 1.25rem;
   text-align: center;
 `;
 
-const CIText2 = styled.p`
+const CiText2 = styled.p`
   padding-top: 5px;
   text-align: center;
   font-weight: 400;
@@ -61,54 +62,54 @@ const CIText2 = styled.p`
 
 function IntroduceCI() {
   return (
-    <CISection>
+    <CiSection>
       <Title>경영 이념</Title>
-      <Icon>
-        <CIIconWrap>
-          <CIIconImage number={1} />
-          <CITextWrap>
-            <CIText1>
+      <IconWrap>
+        <CiIconWrap>
+          <CiIconImage number={1} />
+          <CiTextWrap>
+            <CiText1>
               지속적인 서비스 개선
               <br />
-            </CIText1>
-            <CIText2>
+            </CiText1>
+            <CiText2>
               주기적인 팀회의를 통해 보다
               <br />질 높은 서비스 제공
-            </CIText2>
-          </CITextWrap>
-        </CIIconWrap>
-      </Icon>
-      <Icon2>
-        <CIIconWrap>
-          <CIIconImage number={2} />
-          <CITextWrap>
-            <CIText1>
+            </CiText2>
+          </CiTextWrap>
+        </CiIconWrap>
+      </IconWrap>
+      <IconWrap2>
+        <CiIconWrap>
+          <CiIconImage number={2} />
+          <CiTextWrap>
+            <CiText1>
               개개인의 성장
               <br />
-            </CIText1>
-            <CIText2>
+            </CiText1>
+            <CiText2>
               자격증 및 온라인 강의 수강
               <br />
               비용 및 시간 적극 지원
-            </CIText2>
-          </CITextWrap>
-        </CIIconWrap>
-        <CIIconWrap>
-          <CIIconImage number={3} />
-          <CITextWrap>
-            <CIText1>
+            </CiText2>
+          </CiTextWrap>
+        </CiIconWrap>
+        <CiIconWrap>
+          <CiIconImage number={3} />
+          <CiTextWrap>
+            <CiText1>
               개개인의 성장
               <br />
-            </CIText1>
-            <CIText2>
+            </CiText1>
+            <CiText2>
               주마다 갖는 회의와 자유로운 분위기
               <br />
               에서의 아이디어 창출 및 팀워크 상승
-            </CIText2>
-          </CITextWrap>
-        </CIIconWrap>
-      </Icon2>
-    </CISection>
+            </CiText2>
+          </CiTextWrap>
+        </CiIconWrap>
+      </IconWrap2>
+    </CiSection>
   );
 }
 
