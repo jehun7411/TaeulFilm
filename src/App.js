ab29d9cda64e72c93ac1e2728e6e6e1ef04e2bf1
KyeongBin <aa0715@naver.com>
2021-10-18 04:43:27 +0900

    [Feat : new ] Deving for Faq page markup

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React from "react";
 import { Switch, Route } from "react-router";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import { Business, Main, Faq, Introduce, Product } from "./components/pages";
-
+import {
+  Business,
+  Main,
+  Introduce,
+  Product,
+  Faq,
+  FaqEditPage,
+  FaqPreviewPage,
+} from "./components/pages";
 function App() {
   return (
     <div>
@@ -12,8 +19,10 @@ function App() {
         <Route exact path="/" component={Main} />
         <Route exact path="/business" component={Business} />
         <Route exact path="/faq" component={Faq} />
+        <Route exact path="/Faqeditpage" component={FaqEditPage} />
         <Route exact path="/introduce" component={Introduce} />
         <Route exact path="/product" component={Product} />
+        <Route exact path="/FaqPreviewPage" component={FaqPreviewPage} />
       </Switch>
       <Footer />
     </div>
