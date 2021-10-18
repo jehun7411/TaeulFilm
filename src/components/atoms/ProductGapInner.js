32815ed3ab11dfa4bb8158708ddcb0623b33e42f
ohjehun <jehun7411@gmail.com>
2021-10-18 16:30:33 +0900

    [Feat : new] Deving for Atoms markup

---
+++ src/components/atoms/ProductGapInner.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const ProductGapInner = styled.div`
+  height: 60px;
+  border: 1px solid black;
+`;
+
+export default ProductGapInner;
---
+++ src/components/atoms/ProductImg.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+const ProductImg = styled.img`
+  width: 349.3px;
+  height: 350px;
+  display: flex;
+  position: relative;
+  box-shadow: rgba(50, 50, 50, 0.35) 0px 5px 15px;
+`;
+
+export default ProductImg;
