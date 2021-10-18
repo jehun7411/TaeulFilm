491a7b220aee74887b0990cc9632f0a7783f1c03
ohjehun <jehun7411@gmail.com>
2021-10-18 16:33:27 +0900

    [Feat : new] Deving for App.js Add

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import React from "react";
 import { Switch, Route } from "react-router";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import { Business, Main, Faq, Introduce, Product } from "./components/pages";
+import {
+  Business,
+  Main,
+  Faq,
+  Introduce,
+  Product,
+  Skpi,
+  Pvdfpi,
+  Petpi,
+  Thinpi,
+  Siliconpi,
+  Adhesivepi,
+} from "./components/pages";
 
 function App() {
   return (
@@ -14,6 +26,12 @@ function App() {
         <Route exact path="/faq" component={Faq} />
         <Route exact path="/introduce" component={Introduce} />
         <Route exact path="/product" component={Product} />
+        <Route exact path="/skpi" component={Skpi} />
+        <Route exact path="/petpi" component={Petpi} />
+        <Route exact path="/thinpi" component={Thinpi} />
+        <Route exact path="/pvdfpi" component={Pvdfpi} />
+        <Route exact path="/siliconpi" component={Siliconpi} />
+        <Route exact path="/adhesivepi" component={Adhesivepi} />
       </Switch>
       <Footer />
     </div>
