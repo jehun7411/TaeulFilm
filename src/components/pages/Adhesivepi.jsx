71b8c80941ad037a3a2297530c98e062de131838
ohjehun <jehun7411@gmail.com>
2021-10-21 00:37:20 +0900

    [Feat : new] Delete Pages

--- src/components/pages/Adhesivepi.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styled from "styled-components";
-
-function Adhesivepi() {
-  console.log("adhesivepi");
-  return <div>Faq</div>;
-}
-
-export default Adhesivepi;
