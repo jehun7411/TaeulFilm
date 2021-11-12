0ab7382ee6f2f520a018360d371484b857f2eae3
KyeongBin <aa0715@naver.com>
2021-11-12 18:46:48 +0900

    Edit filmcolor>>Filmcolor

--- src/components/Film/FilmTape.jsx
+++ src/components/Film/FilmTape.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled, { css } from "styled-components";
-import { filmColor } from "../../asset/filmColor/filmColor";
+import { filmColor } from "../../asset/FilmColor/filmColor";
 
 const FilmTapeList = styled.ul`
   margin-top: 3px;
